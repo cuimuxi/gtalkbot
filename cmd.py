9129f440321e569ce9c4e7937402d40297b2de84
Wang Hui <wh_linux@126.com>
2012-09-25 14:25:21 +0800

    fix invite

---
+++ cmd.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#
+# 生成命令
+
+from db import get_members
+from db import get_nick, get_member
+from db import edit_member
+from db import add_history
+from pyxmpp.all import Message
+from pyxmpp.all import JID
+from pyxmpp.all import Presence
+
+
+DEBUG = True
+
+def send_command(stanza, body):
+    cmd = body[1:]
+    cmds = run_cmd(cmd, stanza)
+
+
+def send_msg(stanza, to_email, body):
+    m=Message(
+        to_jid=JID(to_email),
+        from_jid=stanza.get_to(),
+        stanza_type=stanza.get_type(),
+        body=body)
+    stanza.stream.send(m)
+
+def send_all_msg(stanza, body):
+    frm = stanza.get_from()
+    email = '%s@%s' % (frm.node, frm.domain) 
+    nick = get_nick(email)
+    add_history(email, 'all', body)
+    body = "[%s] %s" % (nick, body)
+    tos = get_members(email)
+    for to in tos:
+        send_msg(stanza, to, body)
+
+
+def send_to_msg(stanza, to, body):
+    frm = stanza.get_from()
+    email = '%s@%s' % (frm.node, frm.domain) 
+    nick = get_nick(email)
+    add_history(email, to, body)
+    body = "[%s 悄悄对你说] %s" % (nick, body)
+    send_msg(stanza, to, body)
+
+
+
+class CommandHandler():
+    """
+        生成命令
+        命令对应着方法
+        比如敲入 -list 则对应list方法
+        命令具有统一接收stanza固定参数和变参**kargs,
+        需要参数自行从kargs里提取
+    """
+    def list(self, stanza, *args):
+        """列出所有成员"""
+        members = get_members()
+        body = []
+        for m in members:
+            r = '%s <%s>' % (m.get('nick'), m.get('email'))
+            body.append(r)
+
+        self._send_cmd_result(stanza, '\n'.join(body))
+
+
+    def msgto(self, stanza, *args):
+        """单独给某用户发消息 eg $msgto nick hello(给nick发送hello)"""
+        if len(args) > 1:
+            nick = args[0]
+            receiver = get_member(nick = nick)
+            body = ' '.join(args[1:])
+            if not receiver:
+               self._send_cmd_result(stanza, "%s 用户不存在" % nick)
+            else:
+                send_to_msg(stanza, receiver, body)
+        else:
+            self.help(stanza, 'mgsto')
+
+
+    def nick(self, stanza, *args):
+        """更改昵称 eg. $nick yournewnickname"""
+        if len(args) == 1:
+            nick = args[0]
+            frm = stanza.get_from()
+            email = "%s@%s" % (frm.node, frm.domain)
+            oldnick = get_nick(email)
+            edit_member(email, nick = nick)
+            body = "%s 更改昵称为 %s" % (oldnick, nick)
+            send_all_msg(stanza, body)
+        else:
+            self.help(stanza, 'nick')
+            
+
+    def invite(self, stanza, *args):
+        """邀请好友加入 eg. $invite <yourfirendemail>"""
+        if len(args) >= 1:
+            to = args[0]
+            p = Presence(from_jid = stanza.get_to(),
+                         to_jid = JID(to),
+                         stanza_type = 'subscribe')
+            stanza.stream.send(p)
+            p = Presence(from_jid = stanza.get_to(),
+                         to_jid = JID(to),
+                         stanza_type = 'subscribed')
+            stanza.stream.send(p)
+        else:
+            self.help(stanza, 'invite')
+            
+
+    def help(self, stanza, *args):
+        """显示帮助"""
+        if args:
+            func = self._get_cmd(args[0])
+            if func:
+                body ="$%s : %s" % (args[0], func.__doc__)
+            else:
+                body = "$%s : command unknow" % args[0]
+        else:
+            body = []
+            funcs = self._get_cmd()
+            for f in funcs:
+                r = "$%s  %s" % (f.get('name'), f.get('func').__doc__)
+                body.append(r)
+            body = '\n'.join(body)
+        self._send_cmd_result(stanza, body)
+
+        
+    @classmethod
+    def _send_cmd_result(cls, stanza, body):
+        """返回命令结果"""
+        message = Message(
+            to_jid = stanza.get_from(),
+            from_jid = stanza.get_to(),
+            subject = stanza.get_subject(),
+            body = body)
+        stanza.stream.send(message)
+
+        
+    @classmethod
+    def _get_cmd(cls, name = None):
+        if name:
+            return cls.__dict__.get(name)
+        else:
+            r = [{'name':k, 'func':v} for k, v in cls.__dict__.items() if not k.startswith('_')]
+            return r
+                    
+    
+    def __getattr__(self, name):
+        return self.help
+
+    @classmethod
+    def _run_cmd(cls,cmd, stanza):
+        """获取命令"""
+        args = []
+        c = ''
+        for i, v in enumerate(cmd.split(' ')):
+            if i == 0:
+                c = v
+            else:
+                args.append(v)
+        if DEBUG:
+            cls.__dict__.get(c)(cls, stanza, *args)
+        else:
+            try:
+                cls.__dict__.get(c)(cls, stanza, *args)
+            except:
+                cls.__dict__.get('help')(cls, stanza, c)
+            
+
+
+run_cmd = CommandHandler._run_cmd
